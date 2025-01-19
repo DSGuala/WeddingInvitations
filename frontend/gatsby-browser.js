fcf8c7d1d9a3be53cd178739f48ca1f23cfe24a6
Diego <diego_guala@hotmail.com>
2025-01-18 23:35:09 -0500

    removed frontend folder

--- frontend/gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import "./src/styles/global.css"
-export const onClientEntry = () => {
-    const link = document.createElement("link");
-    link.href = "https://fonts.googleapis.com/css2?family=Charmonman:wght@400;700&family=Metal&family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Rouge+Script&display=swap";
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
-  };
